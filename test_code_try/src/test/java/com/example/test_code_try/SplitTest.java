6048f485b3885ac1099a7a31d7a5dcd7e51cb296
202015013 <101418352+202015013@users.noreply.github.com>
2022-11-11 13:14:40 +0900

    필요없는 코드 삭제 및 코드 추가
    문제와 상관없는 코드 삭제

--- test_code_try/src/test/java/com/example/test_code_try/SplitTest.java
+++ test_code_try/src/test/java/com/example/test_code_try/SplitTest.java
@@ -4,12 +4,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SplitTest {
-
-    SplitNum splitNum = new SplitNum();
-
     @Test
-    void testSplit() {
-        Assertions.assertThat("1,2".split(",")).contains("1");
+    void 쉼표로_나누기_테스트() {
+        Assertions.assertThat("1,2".split(",")).contains("1","2","2");
+        Assertions.assertThat("1".split(",")).containsExactly("1");
+        Assertions.assertThat("1,2,3".split(",")).containsExactly("1","2","3");
     }
 }
 
--- test_code_try/src/test/java/com/example/test_code_try/SubStringTest.java
+++ test_code_try/src/test/java/com/example/test_code_try/SubStringTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class SubStringTest {
 
     @Test
-    void main() {
+    void 괄호_없애기_테스트() {
         String num = "(1,2)";
         assertEquals(num.substring(1,4),"1,2");
     }
